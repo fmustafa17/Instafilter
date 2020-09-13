9510a94ab0c9b5f45bc1189e84684db5bcdd25af
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-09-13 16:18:12 -0700

    adding title and image

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Instafilter - Core Image Filters Program"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
     @IBAction func changeFilter(_ sender: Any) {
         
     }
